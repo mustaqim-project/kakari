5e39ba7b3b1c0c2540d7588c071dcadcb660ac3f
Agung Mustaqim <agungmustaqim15@gmail.com>
2025-05-14 06:05:00 +0700

    asdf234

--- resources/views/frontend/news.blade.php
+++ resources/views/frontend/news.blade.php
@@ -26,31 +26,32 @@
 
 
                 <div class="col-md-8">
+                    <!-- Search Form -->
                     <div class="blog_page_search">
                         <form action="{{ route('news') }}" method="GET">
                             <div class="row">
                                 <div class="col-lg-5">
                                     <input type="text" placeholder="Type here" value="{{ request()->search }}"
-                                        name="search">
+                                        name="search" class="search-input">
                                 </div>
                                 <div class="col-lg-4">
-                                    <select name="category">
+                                    <select name="category" class="category-select">
                                         <option value="">{{ __('frontend.All') }}</option>
                                         @foreach ($categories as $category)
                                             <option {{ $category->slug === request()->category ? 'selected' : '' }}
                                                 value="{{ $category->slug }}">{{ $category->name }}</option>
                                         @endforeach
-
                                     </select>
                                 </div>
                                 <div class="col-lg-3">
-                                    <button type="submit">{{ __('frontend.search') }}</button>
+                                    <button type="submit" class="search-button">{{ __('frontend.search') }}</button>
                                 </div>
                             </div>
                         </form>
                     </div>
 
-                    <aside class="wrapper__list__article ">
+                    <!-- Article List -->
+                    <aside class="wrapper__list__article">
                         @if (request()->has('category'))
                             <h4 class="border_section">{{ __('frontend.Category') }}: {{ request()->category }}</h4>
                         @endif
@@ -62,14 +63,15 @@
                                     <div class="article__entry">
                                         <div class="article__image">
                                             <a href="{{ route('news-details', $post->slug) }}">
-                                                <img src="{{ asset($post->image) }}" alt="" class="img-fluid">
+                                                <img src="{{ asset($post->image) }}" alt=""
+                                                    class="img-fluid article-image">
                                             </a>
                                         </div>
                                         <div class="article__content">
-                                            <div class="article__category">
+                                            <div class="article__category category-tag">
                                                 {{ $post->category->name }}
                                             </div>
-                                            <ul class="list-inline">
+                                            <ul class="list-inline article-meta">
                                                 <li class="list-inline-item">
                                                     <span class="text-primary">
                                                         {{ __('frontend.by') }} {{ $post->auther->name }}
@@ -80,36 +82,36 @@
                                                         {{ date('M d, Y', strtotime($post->created_at)) }}
                                                     </span>
                                                 </li>
-
                                             </ul>
-                                            <h5>
+                                            <h5 class="article-title">
                                                 <a href="{{ route('news-details', $post->slug) }}">
                                                     {!! truncate($post->title) !!}
                                                 </a>
                                             </h5>
-                                            <p>
+                                            <p class="article-excerpt">
                                                 {!! truncate($post->content, 100) !!}
                                             </p>
                                             <a href="{{ route('news-details', $post->slug) }}"
-                                                class="btn btn-outline-primary mb-4 text-capitalize">
+                                                class="btn btn-outline-primary mb-4 text-capitalize read-more">
                                                 {{ __('frontend.read more') }}</a>
                                         </div>
                                     </div>
                                 </div>
                             @endforeach
                             @if (count($news) === 0)
-                                <div class="text-center w-100">
+                                <div class="text-center w-100 no-results">
                                     <h4>{{ __('frontend.No News Found') }} :(</h4>
                                 </div>
                             @endif
                         </div>
-
                     </aside>
-                    <div class="text-center" style="display: flex; justify-content: center;">
-                        <!-- Pagination -->
+
+                    <!-- Pagination -->
+                    <div class="text-center pagination-container">
                         {{ $news->appends(request()->query())->links() }}
                     </div>
                 </div>
+
                 <div class="col-lg-4">
                     <!-- Search Widget -->
                     @include('frontend.components.sidebar.search-widget')
