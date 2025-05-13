312e55641147befd88e10cbef3f84ed64732ec43
Agung Mustaqim <agungmustaqim15@gmail.com>
2025-05-14 01:59:51 +0700

    asdf234

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -2,10 +2,12 @@
 <header>
     <nav class="navbar navbar-expand-lg navbar-light sticky-top">
         <div class="container">
-            <a href="{{ url('/') }}">
-                <img src="{{ asset($settings['site_logo']) }}" class="img-fluid logo" alt="ic-syekhburhanuddin Logo"
-                    aria-label="Go to ic-syekhburhanuddin">
-            </a>
+            <figure class="mb-0 mx-auto">
+                <a href="{{ url('/') }}">
+                    <img src="{{ asset($settings['site_logo']) }}" class="img-fluid logo"
+                        aria-label="Go to ic-syekhburhanuddin" alt="ic-syekhburhanuddin Logo">
+                </a>
+            </figure>
 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
