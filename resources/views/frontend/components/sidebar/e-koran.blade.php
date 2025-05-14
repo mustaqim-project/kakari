e9eb05f9ccf3ee18481ab7f9a3c1899c56b8d7c1
Agung Mustaqim <agungmustaqim15@gmail.com>
2025-05-14 10:07:38 +0700

    asdf234

--- resources/views/frontend/components/sidebar/e-koran.blade.php
+++ resources/views/frontend/components/sidebar/e-koran.blade.php
@@ -5,7 +5,7 @@
     </div>
     <div class="card-body text-center p-0">
         @if ($latestEkoran->count() > 0)
-            <img src="{{ asset('storage/ekoran/' . $latestEkoran->first()->cover_image) }}"
+            <img src="{{ asset($ekoran->image) }}"
                 class="img-fluid cursor-pointer" alt="E-Koran Edisi Terbaru"
                 style="max-height: 300px; object-fit: cover; margin: 2.5em;"
                 data-bs-toggle="modal" data-bs-target="#ekoranModal">
