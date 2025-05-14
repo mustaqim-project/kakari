f162f40774be2be9bcb111599d1a823222de853c
Agung Mustaqim <agungmustaqim15@gmail.com>
2025-05-14 10:58:07 +0700

    ASDF234

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,50 +2,69 @@
 
 @section('content')
     <!-- login -->
-    <section class="wrap__section">
+    <section class="wrap__section bg-light py-5">
         <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="card mx-auto" style="max-width: 380px;">
-                        <div class="card-body">
-                            <h4 class="card-title mb-4">{{ __('frontend.Sign in') }}</h4>
+            <div class="row justify-content-center">
+                <div class="col-md-6 col-lg-5">
+                    <div class="card shadow-sm border-0">
+                        <div class="card-header bg-white border-0 py-3">
+                            <h4 class="card-title mb-0 text-center text-primary">{{ __('frontend.Sign in') }}</h4>
+                        </div>
+                        <div class="card-body px-4 py-4">
                             <form method="POST" action="{{ route('login') }}">
                                 @csrf
-                                {{-- <a href="#" class="btn btn-facebook btn-block mb-2 text-white"> <i
-                                        class="fa fa-facebook"></i> &nbsp; Sign
-                                    in
-                                    with
-                                    Facebook</a>
-                                <a href="#" class="btn btn-primary btn-block mb-4"> <i class="fa fa-google"></i>
-                                    &nbsp;
-                                    Sign in with
-                                    Google</a> --}}
-                                <div class="form-group">
-                                    <input class="form-control" placeholder="{{ __('frontend.Email') }}" type="text" name="email">
+                                <div class="form-group mb-3">
+                                    <label for="email" class="form-label small text-muted">{{ __('frontend.Email') }}</label>
+                                    <input class="form-control form-control-lg rounded-pill @error('email') is-invalid @enderror"
+                                           placeholder="your@email.com"
+                                           type="email"
+                                           name="email"
+                                           id="email"
+                                           required
+                                           autofocus>
                                     @error('email')
-                                        <p class="text-danger">{{ $message }}</p>
+                                        <div class="invalid-feedback">{{ $message }}</div>
                                     @enderror
                                 </div>
-                                <div class="form-group">
-                                    <input class="form-control" placeholder="{{ __('frontend.Password') }}" type="password" name="password">
+
+                                <div class="form-group mb-3">
+                                    <label for="password" class="form-label small text-muted">{{ __('frontend.Password') }}</label>
+                                    <input class="form-control form-control-lg rounded-pill @error('password') is-invalid @enderror"
+                                           placeholder="••••••••"
+                                           type="password"
+                                           name="password"
+                                           id="password"
+                                           required>
                                 </div>
 
-                                <div class="form-group">
-                                    <a href="{{ route('password.request') }}" class="float-right">{{ __('frontend.Forgot password?') }}</a>
-                                    <label class="float-left custom-control custom-checkbox">
-                                        <input type="checkbox" name="remember"
-                                            class="custom-control-input">
-                                        <span class="custom-control-label"> {{ __('frontend.Remember') }} </span>
-                                    </label>
+                                <div class="form-group mb-4 d-flex justify-content-between align-items-center">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" name="remember" id="remember">
+                                        <label class="form-check-label small" for="remember">
+                                            {{ __('frontend.Remember me') }}
+                                        </label>
+                                    </div>
+                                    <a href="{{ route('password.request') }}" class="small text-decoration-none">
+                                        {{ __('frontend.Forgot password?') }}
+                                    </a>
                                 </div>
-                                <div class="form-group">
-                                    <button type="submit" class="btn btn-primary btn-block"> {{ __('frontend.Login') }} </button>
+
+                                <div class="form-group mb-3">
+                                    <button type="submit" class="btn btn-primary btn-lg w-100 rounded-pill py-2 fw-bold">
+                                        {{ __('frontend.Login') }}
+                                    </button>
+                                </div>
+
+                                <div class="text-center mt-4 pt-3 border-top">
+                                    <p class="small text-muted mb-0">{{ __('frontend.Dont have account?') }}
+                                        <a href="{{ route('register') }}" class="text-primary fw-semibold text-decoration-none">
+                                            {{ __('frontend.Sign up') }}
+                                        </a>
+                                    </p>
                                 </div>
                             </form>
                         </div>
                     </div>
-
-                    <p class="text-center mt-4 mb-0">{{ __('frontend.Dont have account?') }} <a href="{{ route('register') }}">{{ __('frontend.Sign up') }}</a></p>
                 </div>
             </div>
         </div>
